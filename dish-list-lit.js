c1e7922fd7b93f10b519d53a34e032f7d03cfb86
Jorge <jorge.cast.cont@gmail.com>
2019-11-24 22:22:47 -0600

    Initial commit

---
+++ dish-list-lit.js
@@ -0,0 +1,33 @@
+import {html, LitElement} from 'lit-element';
+import 'dish-detail-lit/dish-detail-lit';
+import style from './dish-list-styles';
+
+class DishListLit extends LitElement{
+    constructor() {
+        super();
+        this.dishes = [];
+    }
+
+    static get properties() {
+        return {
+            dishes: {
+                type: Array,
+                reflect: true
+            }
+        }
+    }
+
+    static get styles() {
+        return style;
+    }
+
+    render(){
+        return html`
+            <div class="dishes-container">
+                ${this.dishes.map(dish => html`<dish-detail-lit .dish="${dish}" showbuttons></dish-detail-lit>`)}      
+            </div>
+        `;
+    }
+}
+
+window.customElements.define('dish-list-lit', DishListLit);
---
+++ dish-list-styles.js
@@ -0,0 +1,11 @@
+import {css} from 'lit-element';
+
+export default css`
+    :host {
+        display: block;
+    }
+    
+    dish-detail-lit {
+        display: inline;
+    }
+`;
